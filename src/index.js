c45bd2c9352ad518d42ef4d39945a28643166310
Ale Figueroa <alejandro@progressly.com>
2017-06-12 14:28:04 -0500

    Allow custom function key

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ const CacheMap = require('cache-map').default
 const _promise = require('bluebird')
 const safeStringify = require('fast-safe-stringify')
 const debug = require('debug')('memoizep')
+const crypto = require('crypto')
 
 const PromiseLib = Promise === undefined ? _promise : Promise
 
-const memoizePromise = (fn, options) => {
+function cacheKeyfn(...args) {
+  return crypto.createHash('md5').update(safeStringify(args)).digest('hex')
+}
+
+const memoizePromise = (fn, options, keyFn = cacheKeyfn) => {
   const cacheMap = new CacheMap(options.ttl, options.evictInterval)
   const memoizedPromise = (...args) => {
-    const cacheKey = safeStringify(args);
-    debug(cacheKey);
-    const cachedResult = cacheMap.get(cacheKey);
+    const cacheKey = keyFn(...args);
+    debug(cacheKey)
+    const cachedResult = cacheMap.get(cacheKey)
     if (cachedResult) {
-      debug('hit with ' + cacheKey);
+      debug('hit with ' + cacheKey)
       return PromiseLib.resolve(cachedResult)
     }
     return fn(...args).then(result => {
