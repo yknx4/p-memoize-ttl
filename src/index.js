52ecaa150b6719b3aebbf6075fb81398cf1f1b37
Ale Figueroa <alejandro@progressly.com>
2017-06-12 14:04:04 -0500

    Bulletproof

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-const CacheMap = require('cache-map').default;
-const Promise = require('bluebird');
+const CacheMap = require('cache-map').default
+const _promise = require('bluebird')
+const safeStringify = require('fast-safe-stringify')
+const XXHash = require('xxhash')
+
+const PromiseLib = Promise === undefined ? _promise : Promise
 
 const memoizePromise = (fn, options) => {
-    const cacheMap = new CacheMap(options.ttl, options.evictInterval);
-    const memoizedPromise = (...args) => {
-        const cacheKey = args.length ? args.join('__') : '____';
-        const cachedResult = cacheMap.get(cacheKey);
-        if (cachedResult) {
-            return Promise.resolve(cachedResult);
-        }
-        return fn(...args)
-            .then((result) => {
-                cacheMap.set(cacheKey, result);
-                return result;
-            });
-    };
-    return memoizedPromise;
-};
+  const cacheMap = new CacheMap(options.ttl, options.evictInterval)
+  const memoizedPromise = (...args) => {
+    const hasher = new XXHash(0xcafebabe);
+    hasher.update(safeStringify(args));
+    const cacheKey = hasher.digest();
+    const cachedResult = cacheMap.get(cacheKey)
+    if (cachedResult) {
+      return PromiseLib.resolve(cachedResult)
+    }
+    return fn(...args).then(result => {
+      cacheMap.set(cacheKey, result)
+      return result
+    })
+  }
+  return memoizedPromise
+}
 
-module.exports = memoizePromise;
+module.exports = memoizePromise
