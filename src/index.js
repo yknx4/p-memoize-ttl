a0893e805ae834e1d6840196a3ad58290df6d493
Ale Figueroa <alejandro@progressly.com>
2017-06-12 14:17:24 -0500

    Add debug

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ const CacheMap = require('cache-map').default
 const _promise = require('bluebird')
 const safeStringify = require('fast-safe-stringify')
 const XXHash = require('xxhash')
+const debug = require('debug')('memoize-p')
 
 const PromiseLib = Promise === undefined ? _promise : Promise
 
 const memoizePromise = (fn, options) => {
   const cacheMap = new CacheMap(options.ttl, options.evictInterval)
   const memoizedPromise = (...args) => {
-    const hasher = new XXHash(0xcafebabe);
-    hasher.update(Buffer.from(safeStringify(args), 'utf8'));
-    const cacheKey = hasher.digest();
+    const hasher = new XXHash(0xcafebabe)
+    hasher.update(Buffer.from(safeStringify(args), 'utf8'))
+    const cacheKey = hasher.digest()
     const cachedResult = cacheMap.get(cacheKey)
     if (cachedResult) {
+      debug('hit with ' + cacheKey);
       return PromiseLib.resolve(cachedResult)
     }
     return fn(...args).then(result => {
